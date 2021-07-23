8bf4e7d3aa0613623e8ccd2ec062a32764bb725a
MarritNL <86615967+MarritNL@users.noreply.github.com>
2021-07-23 10:07:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('hallo winc academy');
+// dit is een grote som
+/* console.log('multiplyOne', 1230941 * 1823794);
+console.log('divideOne', 637263 / 54); */
+// 1 multiplyOne 2244982810154
+// 2 divideOne 11801.166666666666
+// 3 check
+// 4 nee, omdat het een cooment is
+// 5 check
+// 6 nee, omdat het een cooment is
